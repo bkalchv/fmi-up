d21db8bd3a79101ed108f974ce5f7b69c169f83b
Bogdan Kalchev <bkaltchev98@gmail.com>
2020-12-15 15:15:38 +0200

    Put 1st homework tasks in a folder

---
+++ Homework1/homework1-task1-WOS.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+
+unsigned int calculateArea(unsigned int height, unsigned int length) {
+        return height*length;
+}
+
+int calculateMostDRP_x(const int& mostDLP_x, const unsigned int& length) {
+        return mostDLP_x + length;
+}
+
+int calculateMostDRP_y(const int& mostDLP_y) {
+        return mostDLP_y;
+}
+
+int calculateMostULP_x(const int& mostDLP_x)  {
+        return mostDLP_x;
+}
+
+int calculateMostULP_y(const int& mostDLP_y, const unsigned int& height)  {
+        return mostDLP_y + height;
+}
+
+int calculateMostURP_x(const int& mostDLP_x, const unsigned int& length)  {
+        return mostDLP_x + length;
+}
+
+int calculateMostURP_y(const int& mostDLP_y, const unsigned int& height)  {
+        return mostDLP_y + height;
+}
+
+bool doOverlay(const int& rec1_x1, const int& rec1_y1, const unsigned int& rec1_length, const unsigned int& rec1_height, const int& rec2_x2, const int& rec2_y2, const unsigned int& rec2_length, const unsigned int& rec2_height) {
+    
+    if (rec1_x1 >= calculateMostURP_x(rec2_x2, rec2_length) || calculateMostURP_x(rec1_x1, rec1_length) <= rec2_x2)
+    {
+        return false;
+    }
+
+    if (rec1_y1 >= calculateMostURP_y(rec2_y2, rec2_height) || calculateMostURP_y(rec1_y1, rec1_height) <= rec2_y2)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+unsigned int calculateOverlayArea(const int& rec1_x1, const int& rec1_y1, const unsigned int& rec1_length, const unsigned int& rec1_height, const int& rec2_x2, const int& rec2_y2, const unsigned int& rec2_length, const unsigned int& rec2_height) {
+
+    if (doOverlay(rec1_x1, rec1_y1, rec1_length, rec1_height, rec2_x2, rec2_y2, rec2_length, rec2_height))
+    {   
+        int overlayLenght;
+        
+        int overlayMostURP_x;
+        int overlayMostDLP_x;
+
+        if (calculateMostURP_x(rec1_x1, rec1_length) < calculateMostURP_x(rec2_x2, rec2_length)) 
+        {
+            overlayMostURP_x = calculateMostURP_x(rec1_x1, rec1_length);
+        }
+        else 
+        {
+            overlayMostURP_x = calculateMostURP_x(rec2_x2, rec2_length);
+        }
+
+        if (rec1_y1 > rec2_y2)
+        {
+            overlayMostDLP_x = rec1_x1;
+        }
+        else
+        {
+            overlayMostDLP_x = rec2_x2;
+        }
+        
+        overlayLenght = overlayMostURP_x - overlayMostDLP_x;
+
+        int overlayHeight;
+
+        int overlayMostURP_y;
+        int overlayMostDLP_y;
+
+        if (calculateMostURP_y(rec1_y1, rec1_height) < calculateMostURP_y(rec2_y2, rec2_height)) 
+        {
+            overlayMostURP_y = calculateMostURP_y(rec1_y1, rec1_height);
+        }
+        else 
+        {
+            overlayMostURP_y = calculateMostURP_y(rec2_y2, rec2_height);
+        }
+
+        if (rec1_x1 > rec2_x2)
+        {
+            overlayMostDLP_y = rec1_y1;
+        }
+        else
+        {
+            overlayMostDLP_y = rec2_y2;
+        }
+        
+        overlayHeight = overlayMostURP_y - overlayMostDLP_y;
+
+        return overlayLenght * overlayHeight;
+    }
+
+    return 0;
+}
+
+int main() 
+{
+    int x1, y1;
+    size_t height1, length1;
+    cout << "Input data for the first rectangle: ";
+    cin >> x1 >> y1 >> height1 >> length1;
+
+    int x2, y2;
+    size_t height2, length2;
+    cout << "Input data for the second rectangle: ";
+    cin >> x2 >> y2 >> height2 >> length2;
+
+    cout << "Rec1 and Rec2";
+
+    bool rectanglesDoOverlay = doOverlay(x1, y1, length1, height1, x2, y2, length2, height2);
+
+    if (rectanglesDoOverlay)
+    {
+        cout << " do overlay.\n";
+    }
+    else
+    {
+        cout << "do NOT overlay\n";
+    }
+    
+    cout << "Area of overlay: ";
+    cout << calculateOverlayArea(x1, y1, length1, height1, x2, y2, length2, height2) << endl; 
+
+    return 0;
+}
---
+++ Homework1/homework1-task1.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+
+    Point() {
+        x = 0;
+        y = 0;
+    }
+
+    Point(int _x, int _y) {
+        x = _x;
+        y = _y;
+    }
+};
+
+struct Rectangle
+{   
+    size_t  length;
+    size_t  height;
+    Point   mostDLP;
+
+    Rectangle(size_t _length, size_t _height, Point _mostDLP = Point()) {
+        length = _length;
+        height = _height;
+        mostDLP = _mostDLP;
+    }
+
+    unsigned int calculateArea() {
+        return height*length;
+    }
+
+    Point calculateMostDRP() const {
+        return Point(mostDLP.x + length, mostDLP.y);
+    }
+
+    Point calculateMostULP() const {
+        return Point(mostDLP.x, mostDLP.y + height);
+    }
+
+    Point calculateMostURP() const {
+        return Point(mostDLP.x + length, mostDLP.y + height);
+    }
+};
+
+// vector<Point> getAllPoints(const Rectangle& rec) {
+//     vector<Point> result;
+//     result.reserve(4);
+//     result = {rec.mostDLP, rec.calculateMostDRP() , rec.calculateMostULP(),  rec.calculateMostURP()};
+//     return result;
+// }
+
+bool doOverlay(const Rectangle& rec1, const Rectangle& rec2) {
+    
+    if (rec1.mostDLP.x >= rec2.calculateMostURP().x || rec1.calculateMostURP().x <= rec2.mostDLP.x)
+    {
+        return false;
+    }
+
+    if (rec1.mostDLP.y >= rec2.calculateMostURP().y || rec1.calculateMostURP().y <= rec2.mostDLP.y)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+unsigned int calculateOverlayArea(const Rectangle& rec1, const Rectangle& rec2) {
+
+    if (doOverlay(rec1, rec2)) 
+    {   
+        int overlayLenght;
+        
+        int overlayMostUR_x;
+        int overlayMostDL_x;
+
+        if (rec1.calculateMostURP().x < rec2.calculateMostURP().x) 
+        {
+            overlayMostUR_x = rec1.calculateMostURP().x;
+        }
+        else 
+        {
+            overlayMostUR_x = rec2.calculateMostURP().x;
+        }
+
+        if (rec1.mostDLP.y > rec1.mostDLP.y)
+        {
+            overlayMostDL_x = rec1.mostDLP.x;
+        }
+        else
+        {
+            overlayMostDL_x = rec2.mostDLP.x;
+        }
+        
+        overlayLenght = overlayMostUR_x - overlayMostDL_x;
+
+        int overlayHeight;
+
+        int overlayMostUR_y;
+        int overlayMostDL_y;
+
+        if (rec1.calculateMostURP().y < rec2.calculateMostURP().y) 
+        {
+            overlayMostUR_y = rec1.calculateMostURP().y;
+        }
+        else 
+        {
+            overlayMostUR_y = rec2.calculateMostURP().y;
+        }
+
+        if (rec1.mostDLP.x > rec2.mostDLP.x)
+        {
+            overlayMostDL_y = rec1.mostDLP.y;
+        }
+        else
+        {
+            overlayMostDL_y = rec2.mostDLP.y;
+        }
+        
+        overlayHeight = overlayMostUR_y - overlayMostDL_y;
+
+        return overlayLenght * overlayHeight;
+    }
+
+    return 0;
+}
+
+int main() 
+{
+    int x1, y1;
+    size_t height1, length1;
+    cout << "Input data for the first rectangle: ";
+    cin >> x1 >> y1 >> height1 >> length1;
+    Rectangle rec1 = Rectangle(height1, length1, Point(x1,y1));
+
+    int x2, y2;
+    size_t height2, length2;
+    cout << "Input data for the second rectangle: ";
+    cin >> x2 >> y2 >> height2 >> length2;
+    Rectangle rec2 = Rectangle(height2, length2, Point(x2,y2));
+
+    cout << "Rec1 and Rec2";
+    doOverlay(rec1, rec2) ? cout << " do overlay.\n" : cout << "do not overlay\n";
+    cout << "Area of overlay: ";
+    doOverlay(rec1, rec2) ? cout << calculateOverlayArea(rec1, rec2) << endl : cout << 0 << endl; 
+
+    return 0;
+}
